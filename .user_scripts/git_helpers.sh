340fb8f5dc86a4bbc41f74bfb5be1f444595d6e6
Christian Witts <cwitts@gmail.com>
2017-05-22 10:55:56 +0200

    Update git helpers
    Signed-off-by: Christian Witts <cwitts@gmail.com>

--- .user_scripts/git_helpers.sh
+++ .user_scripts/git_helpers.sh
@@ -3,8 +3,12 @@
 # - Creating pull requests against Stash or GitHub
 # - Releasing your feature into your tracking branch
 
+PR_LINK=''
+JIRA_HOST=''
+# Don't forget to create your ~/.netrc file to login to Jira
+# machine fqdn login username password password
 
-######################################
+#######################################
 # Checkout a new branch, tracking against the
 # currently checked out branch.
 # Globals:
@@ -18,12 +22,30 @@
 #   None
 #######################################
 function code-on {
-    set -e -x
-    local BRANCH=`git rev-parse --abbrev-ref HEAD`
+    set -x
+    local ORIGIN=`git rev-parse --abbrev-ref HEAD`
     local FEATURE_BRANCH=$(whoami)"-${1}"
 
-    git checkout -b $NEWBRANCH origin/${FEATURE_BRANCH}
-    set +e +x
+    git checkout -b ${FEATURE_BRANCH} origin/${ORIGIN}
+    set +x
+}
+
+#######################################
+# Push your code up
+# Globals:
+#   None
+# Locals:
+#   BRANCH          => Current branch you're working on
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+function code-push {
+    set -x
+    local BRANCH=`git rev-parse --abbrev-ref HEAD`
+    git push origin HEAD:${BRANCH}
+    set +x
 }
 
 #######################################
@@ -41,11 +63,9 @@ function code-on {
 #   None
 #######################################
 function code-pr-stash {
-    set -e -x
+    set -x
     local BRANCH=`git rev-parse --abbrev-ref HEAD`
-    local TRACKING_BRANCH=`git rev-parse --abbrev-ref
---symbolic-full-name @{upstream} | awk '{sub(/\//," ")}1' | awk '{print
-$2}'`
+    local TRACKING_BRANCH=`git rev-parse --abbrev-ref --symbolic-full-name @{upstream} | awk '{sub(/\//," ")}1' | awk '{print $2}'`
     local REVIEWER_LIST=()
     for REVIEWER in "$@"
     do
@@ -55,7 +75,7 @@ $2}'`
         REVIEWER_LIST+="${REVIEWER} "
     done
     stash pull-request ${BRANCH} ${TRACKING_BRANCH} ${REVIEWER_LIST[@]}
-    set +e +x
+    set +x
 }
 
 #######################################
@@ -70,11 +90,55 @@ $2}'`
 #   None
 #######################################
 function code-pr-github {
-    set -e -x
+    set -x
     local BRANCH=`git rev-parse --abbrev-ref HEAD`
-    git push -u origin HEAD:${BRANCH}
-    hub pull-request -h "${BRANCH}" -F -
-    set +e +x
+    local TRACKING_BRANCH=`git rev-parse --abbrev-ref --symbolic-full-name @{upstream} | awk '{sub(/\//," ")}1' | awk '{print $2}'`
+    local MESSAGE=`git log --oneline -1 --pretty=%s`
+    git push origin HEAD:${BRANCH}
+    PR_LINK=$(echo "${MESSAGE}" | hub pull-request -b "${TRACKING_BRANCH}" -h "${BRANCH}" -F -)
+    echo $PR_LINK
+    set +x
+}
+
+#######################################
+# Link your Pull Request to a JIRA issue
+# Globals:
+#   None
+# Locals:
+#   LINK_TITLE => The first line of your commit
+#   ISSUE      => The issue#, derived from the branch name
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+function link-pr {
+    set -x
+    local LINK_TITLE=`git log --oneline -1 --pretty=%s`
+    local ISSUE=`git rev-parse --abbrev-ref HEAD | sed 's/'$(whoami)-'//'`
+    curl -n -XPOST -H "Content-Type: application/json" \
+        ${JIRA_HOST}/rest/api/latest/issue/${ISSUE}/remotelink -d \
+            '{"object": {"url": "'${PR_LINK}'", "title": "'${LINK_TITLE}'"}}'
+    set +x
+}
+
+#######################################
+# Create a PR
+# Globals:
+#   None
+# Locals:
+#   BRANCH          => Current branch you're working on
+#   TRACKING_BRANCH => The upstream tracking branch you're merging to
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+function code-pr {
+    set -x
+    code-pr-github
+    link-pr
+    set +x
 }
 
 #######################################
@@ -98,11 +162,9 @@ function code-pr-github {
 #   None
 #######################################
 function code-release {
-    set -e -x
+    set -x
     local BRANCH=`git rev-parse --abbrev-ref HEAD`
-    local TRACKING_BRANCH=`git rev-parse --abbrev-ref
---symbolic-full-name @{upstream} | awk '{sub(/\//," ")}1' | awk '{print
-$2}'`
+    local TRACKING_BRANCH=`git rev-parse --abbrev-ref --symbolic-full-name @{upstream} | awk '{sub(/\//," ")}1' | awk '{print $2}'`
     git pull --rebase
     git rebase -i
     git push origin HEAD:${BRANCH} -f
@@ -111,6 +173,5 @@ $2}'`
     git checkout ${TRACKING_BRANCH}
     git pull
     git branch -d ${BRANCH}
-    set +e +x
+    set +x
 }
-
